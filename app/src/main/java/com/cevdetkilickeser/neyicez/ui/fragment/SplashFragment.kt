ba003ef5d0ad78e25eb091a9f0ee95edc3293bb7
Cevdet Kilickeser <kilickesercevdet@gmail.com>
2024-10-10 08:55:14 +0300

    SplashFragment - Comments deleted

--- app/src/main/java/com/cevdetkilickeser/neyicez/ui/fragment/SplashFragment.kt
+++ app/src/main/java/com/cevdetkilickeser/neyicez/ui/fragment/SplashFragment.kt
@@ -4,15 +4,15 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.os.Handler
 import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.cevdetkilickeser.neyicez.R
-import android.os.Handler
 import androidx.navigation.Navigation
 import com.cevdetkilickeser.neyicez.MainActivity
+import com.cevdetkilickeser.neyicez.R
 import com.cevdetkilickeser.neyicez.databinding.FragmentSplashBinding
 import com.google.firebase.auth.FirebaseAuth
 
@@ -22,31 +22,23 @@ class SplashFragment : Fragment() {
     private lateinit var auth: FirebaseAuth
     private lateinit var appPref: SharedPreferences
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
         binding = FragmentSplashBinding.inflate(inflater, container, false)
         auth = FirebaseAuth.getInstance()
         appPref = requireActivity().getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
-        val introStatu = appPref.getBoolean("introStatu", false)
+        val introStatus = appPref.getBoolean("introStatus", false)
+        val currentUser = auth.currentUser
+        val isEmailVerified = currentUser?.isEmailVerified ?: false
 
-        /*
-            when {
-                !introStatu -> navigate(R.id.splashToIntro)
-                auth.currentUser == null -> navigate(R.id.splashToLogIn)
-                !auth.currentUser!!.isEmailVerified -> navigate(R.id.splashToUnconfirmed)
-                else -> {
-                    val intent = Intent(requireContext(), MainActivity::class.java)
-                    startActivity(intent)
-                    requireActivity().finish()
-                }
-            }
-            */
-        if (!introStatu) {
+        if (!introStatus) {
             navigate(R.id.splashToIntro)
         } else {
-            if (auth.currentUser == null) {
+            if (currentUser == null) {
                 navigate(R.id.splashToLogIn)
             } else {
-                if (!auth.currentUser!!.isEmailVerified) {
+                if (!isEmailVerified) {
                     navigate(R.id.splashToUnconfirmed)
                 } else {
                     val intent = Intent(requireContext(), MainActivity::class.java)
@@ -56,13 +48,13 @@ class SplashFragment : Fragment() {
             }
         }
 
-    return binding.root
+        return binding.root
 
     }
 
-    private fun navigate(id:Int){
+    private fun navigate(id: Int) {
         Handler(Looper.getMainLooper()).postDelayed({
             Navigation.findNavController(binding.root).navigate(id)
-        },1000)
+        }, 1000)
     }
 }
