3d8a097394a27a485b43a86e5d942af849345058
Cevdet Kilickeser <kilickesercevdet@gmail.com>
2024-10-10 08:56:12 +0300

    IntroFragment - CTRL + L applied

--- app/src/main/java/com/cevdetkilickeser/neyicez/ui/fragment/IntroFragment.kt
+++ app/src/main/java/com/cevdetkilickeser/neyicez/ui/fragment/IntroFragment.kt
@@ -3,42 +3,41 @@ package com.cevdetkilickeser.neyicez.ui.fragment
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import androidx.viewpager.widget.ViewPager
 import com.cevdetkilickeser.neyicez.R
 import com.cevdetkilickeser.neyicez.databinding.FragmentIntroBinding
 import com.cevdetkilickeser.neyicez.ui.adapter.IntroAdapter
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
 
 class IntroFragment : Fragment() {
 
     private lateinit var binding: FragmentIntroBinding
     private lateinit var appPref: SharedPreferences
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
         binding = FragmentIntroBinding.inflate(inflater, container, false)
         appPref = requireActivity().getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
 
-        val introList = ArrayList<Int>()
-        introList.add(1)
-        introList.add(2)
-        introList.add(3)
+        val introList = listOf(1, 2, 3)
 
-        val introAdapter = IntroAdapter(requireContext(),introList)
+        val introAdapter = IntroAdapter(requireContext(), introList)
         binding.viewPager.adapter = introAdapter
         binding.dotIndicator.attachTo(binding.viewPager)
 
-
-        binding.viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener{
-            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
+        binding.viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrolled(
+                position: Int, positionOffset: Float, positionOffsetPixels: Int
+            ) {
+            }
 
             override fun onPageSelected(position: Int) {
-                if (position == (introList.size-1)){
+                if (position == (introList.size - 1)) {
                     binding.buttonSkip.visibility = View.INVISIBLE
                     binding.buttonStart.visibility = View.VISIBLE
                 }
@@ -58,8 +57,8 @@ class IntroFragment : Fragment() {
         return binding.root
     }
 
-    private fun onClickButton(view: View){
+    private fun onClickButton(view: View) {
         Navigation.findNavController(view).navigate(R.id.introToLogIn)
-        appPref.edit().putBoolean("introStatu", true).apply()
+        appPref.edit().putBoolean("introStatus", true).apply()
     }
 }
