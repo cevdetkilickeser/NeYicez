813621bade1f2455a8cd7f29d30ade0ae3dfd661
Cevdet Kilickeser <kilickesercevdet@gmail.com>
2024-10-15 11:21:52 +0300

    add to cart function moved to fragment with higher order. viewmodel removed from adapter and view holder class parameter

--- app/src/main/java/com/cevdetkilickeser/neyicez/presentation/ui/adapter/FoodsAdapter.kt
+++ app/src/main/java/com/cevdetkilickeser/neyicez/presentation/ui/adapter/FoodsAdapter.kt
@@ -5,19 +5,18 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.cevdetkilickeser.neyicez.data.model.Foods
 import com.cevdetkilickeser.neyicez.databinding.FoodsCardBinding
-import com.cevdetkilickeser.neyicez.presentation.viewmodel.HomeViewModel
 import javax.inject.Inject
 
 class FoodsAdapter @Inject constructor(
-    private val viewModel: HomeViewModel,
     private val foodList: List<Foods>,
-    private val onClickListener: (Foods) -> Unit
+    private val onFoodClickListener: (Foods) -> Unit,
+    private val onAddToCartClickListener: (Foods) -> Unit
 ) : RecyclerView.Adapter<FoodsViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FoodsViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         val binding = FoodsCardBinding.inflate(layoutInflater, parent, false)
-        return FoodsViewHolder(binding, viewModel)
+        return FoodsViewHolder(binding)
     }
 
     override fun getItemCount(): Int {
@@ -25,6 +24,6 @@ class FoodsAdapter @Inject constructor(
     }
 
     override fun onBindViewHolder(holder: FoodsViewHolder, position: Int) {
-        holder.bind(foodList[position], onClickListener)
+        holder.bind(foodList[position], onFoodClickListener, onAddToCartClickListener)
     }
 }
