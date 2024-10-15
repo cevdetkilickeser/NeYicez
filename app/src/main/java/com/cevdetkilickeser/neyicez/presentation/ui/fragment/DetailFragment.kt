e57c1ebb9638fa013fd8822e26374a660b96a9ed
Cevdet Kilickeser <kilickesercevdet@gmail.com>
2024-10-15 12:45:57 +0300

    business logic moved to viewmodel,

--- app/src/main/java/com/cevdetkilickeser/neyicez/presentation/ui/fragment/DetailFragment.kt
+++ app/src/main/java/com/cevdetkilickeser/neyicez/presentation/ui/fragment/DetailFragment.kt
@@ -1,16 +1,16 @@
 package com.cevdetkilickeser.neyicez.presentation.ui.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.cevdetkilickeser.neyicez.R
-import com.cevdetkilickeser.neyicez.data.model.Foods
+import com.cevdetkilickeser.neyicez.data.model.Food
 import com.cevdetkilickeser.neyicez.databinding.FragmentDetailBinding
 import com.cevdetkilickeser.neyicez.presentation.viewmodel.DetailViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -19,39 +19,20 @@ import dagger.hilt.android.AndroidEntryPoint
 class DetailFragment : Fragment() {
     private lateinit var binding: FragmentDetailBinding
     private lateinit var viewModel: DetailViewModel
-    private lateinit var food: Foods
-    private var qty = 1
+    private lateinit var food: Food
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         binding = FragmentDetailBinding.inflate(inflater, container, false)
 
         loadDetailPage()
 
-        binding.imageButtonBack.setOnClickListener {
-            backToHome(it)
-        }
+        initObservers()
 
-        binding.checkBoxFavDetail.setOnCheckedChangeListener { buttonView, isChecked ->
-            if (!isChecked){
-                viewModel.addToFav(food.yemek_adi,food.yemek_resim_adi,food.yemek_fiyat,1)
-                binding.checkBoxFavDetail.isChecked = true
-            }else{
-                viewModel.deleteFromFav(food.yemek_adi)
-                binding.checkBoxFavDetail.isChecked = false
-            }
-        }
-
-        binding.buttonDecrease.setOnClickListener {
-            decreaseQty()
-        }
-
-        binding.buttonIncrease.setOnClickListener {
-            increaseQty()
-        }
-
-        binding.buttonAddToCart.setOnClickListener {
-            addToCart()
-        }
+        initListeners()
 
         return binding.root
     }
@@ -63,35 +44,45 @@ class DetailFragment : Fragment() {
         viewModel = tempViewModel
     }
 
-    private fun loadDetailPage(){
-        val bundle:DetailFragmentArgs by navArgs()
+    private fun initObservers() {
+        viewModel.qty.observe(viewLifecycleOwner) { qty ->
+            with(binding) {
+                textViewQtyDetail.text = viewModel.qty.value.toString()
+                textViewTotalPrcDetail.text = getString(R.string.price_text, food.yemek_fiyat * qty)
+            }
+        }
+    }
+
+    private fun initListeners() {
+        binding.imageButtonBack.setOnClickListener {
+            findNavController().popBackStack()
+        }
+
+        binding.buttonDecrease.setOnClickListener {
+            viewModel.decreaseQty()
+        }
+
+        binding.buttonIncrease.setOnClickListener {
+            viewModel.increaseQty()
+        }
+
+        binding.buttonAddToCart.setOnClickListener {
+            val qty = binding.textViewQtyDetail.text.toString().toInt()
+            viewModel.addToCart(food, qty)
+        }
+    }
+
+    private fun loadDetailPage() {
+        val bundle: DetailFragmentArgs by navArgs()
         food = bundle.food
-        binding.checkBoxFavDetail.isChecked = viewModel.checkFav(food.yemek_adi)
 
-        Glide.with(requireContext()).load("http://kasimadalan.pe.hu/yemekler/resimler/${food.yemek_resim_adi}").into(binding.imageViewDetail)
-        binding.textViewNameDetail.text = food.yemek_adi
-        binding.textViewPriceDetail.text = "₺ ${food.yemek_fiyat}"
-        binding.textViewQtyDetail.text = qty.toString()
-        binding.textViewTotalPrcDetail.text = "₺ ${food.yemek_fiyat}"
-    }
-
-    private fun increaseQty(){
-        if (qty < 10) qty++
-        binding.textViewQtyDetail.text = qty.toString()
-        binding.textViewTotalPrcDetail.text = "₺ ${qty*food.yemek_fiyat}"
-    }
-
-    private fun decreaseQty(){
-        if (qty > 1) qty--
-        binding.textViewQtyDetail.text = qty.toString()
-        binding.textViewTotalPrcDetail.text = "₺ ${qty*food.yemek_fiyat}"
-    }
-
-    private fun addToCart(){
-        viewModel.addToCart(food.yemek_adi,food.yemek_resim_adi,food.yemek_fiyat,qty)
-    }
-
-    private fun backToHome(view:View){
-        Navigation.findNavController(view).navigate(R.id.detailToHome)
+        with(binding) {
+            Glide.with(requireContext())
+                .load("http://kasimadalan.pe.hu/yemekler/resimler/${food.yemek_resim_adi}")
+                .into(imageViewDetail)
+            textViewNameDetail.text = food.yemek_adi
+            textViewPriceDetail.text = getString(R.string.price_text, food.yemek_fiyat)
+            textViewTotalPrcDetail.text = getString(R.string.price_text, food.yemek_fiyat)
+        }
     }
 }
--- app/src/main/java/com/cevdetkilickeser/neyicez/presentation/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/cevdetkilickeser/neyicez/presentation/viewmodel/DetailViewModel.kt
@@ -1,85 +1,72 @@
 package com.cevdetkilickeser.neyicez.presentation.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.cevdetkilickeser.neyicez.data.model.Cart
+import com.cevdetkilickeser.neyicez.data.model.Food
 import com.cevdetkilickeser.neyicez.data.repo.CartRepository
-import com.cevdetkilickeser.neyicez.data.repo.FavRepository
 import com.cevdetkilickeser.neyicez.domain.AuthService
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class DetailViewModel @Inject constructor(
     authService: AuthService,
-    private var cartRepo: CartRepository,
-    var favRepo: FavRepository
+    private var cartRepo: CartRepository
 ) : ViewModel() {
 
     private var userName = authService.auth.currentUser?.email.toString()
 
-    fun addToCart(
-        yemek_adi: String,
-        yemek_resim_adi: String,
-        yemek_fiyat: Int,
-        yemek_siparis_adet: Int
-    ) {
+    private var _qty = MutableLiveData(1)
+    var qty: LiveData<Int> = _qty
+
+    fun increaseQty() {
+        if (_qty.value!! < 10) _qty.value = _qty.value!! + 1
+    }
+
+    fun decreaseQty() {
+        if (_qty.value!! > 1) _qty.value = _qty.value!! - 1
+    }
+
+    fun addToCart(food: Food, yemek_siparis_adet: Int) {
         var existingItem: Cart? = null
-        CoroutineScope(Dispatchers.Main).launch {
+        viewModelScope.launch {
             try {
                 val cartFoods = cartRepo.loadCart(userName)
-                existingItem = cartFoods.find { it.yemek_adi == yemek_adi }
+                existingItem = cartFoods.find { it.yemek_adi == food.yemek_adi }
             } catch (_: Exception) {
             }
-            if (existingItem != null) {
-                val newQuantity = existingItem!!.yemek_siparis_adet + yemek_siparis_adet
-                cartRepo.deleteFromCart(existingItem!!.sepet_yemek_id, userName)
-                cartRepo.addToCart(yemek_adi, yemek_resim_adi, yemek_fiyat, newQuantity, userName)
-            } else {
-                cartRepo.addToCart(
-                    yemek_adi,
-                    yemek_resim_adi,
-                    yemek_fiyat,
-                    yemek_siparis_adet,
-                    userName
-                )
+
+            existingItem?.let {
+                addToCartNew(food, yemek_siparis_adet, existingItem!!)
+            } ?: run {
+                addToCartExist(food, yemek_siparis_adet)
             }
         }
     }
 
-    fun addToFav(
-        yemek_adi: String,
-        yemek_resim_adi: String,
-        yemek_fiyat: Int,
-        yemek_fav_adet: Int
-    ) {
-        CoroutineScope(Dispatchers.Main).launch {
-            //favrepo.addToFav(yemek_adi,yemek_resim_adi,yemek_fiyat,yemek_fav_adet,fkullanici_adi)
-        }
+    private suspend fun addToCartNew(food: Food, yemek_siparis_adet: Int, existingItem: Cart) {
+        val newQuantity = existingItem.yemek_siparis_adet + yemek_siparis_adet
+        cartRepo.deleteFromCart(existingItem.sepet_yemek_id, userName)
+        cartRepo.addToCart(
+            food.yemek_adi,
+            food.yemek_resim_adi,
+            food.yemek_fiyat,
+            newQuantity,
+            userName
+        )
     }
 
-    fun deleteFromFav(yemek_adi: String) {
-        CoroutineScope(Dispatchers.Main).launch {
-//            val favList = favrepo.loadFav(fkullanici_adi)
-//            val existingItem = favList.find { it.yemek_adi == yemek_adi }
-//            favrepo.deleteFromFav(existingItem!!.sepet_yemek_id,fkullanici_adi)
-        }
-    }
-
-    fun checkFav(yemek_adi: String): Boolean {
-        var isFav = false
-//        CoroutineScope(Dispatchers.Main).launch {
-//            var existingItem:Cart?=null
-//            try {
-//                val favList = favrepo.loadFav(fkullanici_adi)
-//                existingItem = favList.find { it.yemek_adi == yemek_adi }
-//            }catch (_:Exception){
-//                isFav = false
-//            }
-//            isFav = existingItem != null
-//        }
-        return isFav
+    private suspend fun addToCartExist(food: Food, yemek_siparis_adet: Int) {
+        cartRepo.addToCart(
+            food.yemek_adi,
+            food.yemek_resim_adi,
+            food.yemek_fiyat,
+            yemek_siparis_adet,
+            userName
+        )
     }
 }
