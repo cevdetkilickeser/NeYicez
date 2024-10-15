48f89f673ae89842161cadf1f929a1825e4d0642
Cevdet Kilickeser <kilickesercevdet@gmail.com>
2024-10-15 16:59:46 +0300

    rearranged codes

--- app/src/main/java/com/cevdetkilickeser/neyicez/presentation/viewmodel/FavsViewModel.kt
+++ app/src/main/java/com/cevdetkilickeser/neyicez/presentation/viewmodel/FavsViewModel.kt
@@ -1,37 +1,34 @@
 package com.cevdetkilickeser.neyicez.presentation.viewmodel
 
-import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.cevdetkilickeser.neyicez.data.model.Cart
+import com.cevdetkilickeser.neyicez.data.model.Fav
 import com.cevdetkilickeser.neyicez.data.repo.CartRepository
 import com.cevdetkilickeser.neyicez.data.repo.FavRepository
+import com.cevdetkilickeser.neyicez.domain.AuthService
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class FavsViewModel @Inject constructor(var cartRepo:CartRepository, var favrepo: FavRepository) : ViewModel() {
+class FavsViewModel @Inject constructor(
+    authService: AuthService,
+    var cartRepo: CartRepository,
+    private var favrepo: FavRepository
+) : ViewModel() {
 
-    var kullanici_adi = com.cevdetkilickeser.neyicez.utils.UserInfo.currentUser!!
-    var fkullanici_adi = "f$kullanici_adi"
-    var favList = MutableLiveData<List<Cart>>()
+    private var username = authService.auth.currentUser?.email.toString()
+
+    private val _favList = MutableLiveData<List<Fav>>()
+    val favList: LiveData<List<Fav>> = _favList
 
     init {
-        loadFavFoods(fkullanici_adi)
-        if (favList.value!=null){
-            Log.e("şş", favList.value!!.size.toString())
-        }else{
-            Log.e("şş", "size boş")}
+        loadFavs()
     }
 
-    fun loadFavFoods(kullanici_fav:String){
-        CoroutineScope(Dispatchers.Main).launch {
-            try {
-//                favList.value = favrepo.loadFav(kullanici_fav)
-            }catch (_:Exception){}
+    private fun loadFavs() {
+        favrepo.getFavs(username) {
+            _favList.value = it ?: emptyList()
         }
     }
 }
