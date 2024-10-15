8ef941894880962a87e59a956b33f5798cdb7383
Cevdet Kilickeser <kilickesercevdet@gmail.com>
2024-10-15 15:48:19 +0300

    Firebase updates applied

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,9 +70,9 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.5.0")
 
     //Firebase
-    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
+    implementation(platform("com.google.firebase:firebase-bom:33.4.0"))
     implementation("com.google.firebase:firebase-auth")
-    implementation("com.google.android.gms:play-services-auth:21.0.0")
+    implementation("com.google.android.gms:play-services-auth:21.2.0")
     implementation("com.google.firebase:firebase-firestore")
     implementation("com.google.firebase:firebase-storage")
 
