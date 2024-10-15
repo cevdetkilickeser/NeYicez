de50b431e1db0d8c9fb7488cc1703250c8d1bfac
Cevdet Kilickeser <kilickesercevdet@gmail.com>
2024-10-15 16:55:20 +0300

    FavsViewHolder moved to out of adapter

--- app/src/main/java/com/cevdetkilickeser/neyicez/presentation/ui/adapter/FavsAdapter.kt
+++ app/src/main/java/com/cevdetkilickeser/neyicez/presentation/ui/adapter/FavsAdapter.kt
@@ -1,37 +1,26 @@
 package com.cevdetkilickeser.neyicez.presentation.ui.adapter
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.cevdetkilickeser.neyicez.R
-import com.cevdetkilickeser.neyicez.data.model.Cart
+import com.cevdetkilickeser.neyicez.data.model.Fav
 import com.cevdetkilickeser.neyicez.databinding.FavCardBinding
-import com.cevdetkilickeser.neyicez.presentation.viewmodel.FavsViewModel
 
-class FavsAdapter(var mContext: Context, var favList: List<Cart>, var viewModel: FavsViewModel) : RecyclerView.Adapter<FavsAdapter.FavCardHolder>() {
+class FavsAdapter(
+    private var favList: List<Fav>
+) : RecyclerView.Adapter<FavsViewHolder>() {
 
-    inner class FavCardHolder(var binding: FavCardBinding) : RecyclerView.ViewHolder(binding.root)
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavCardHolder {
-        val layoutInflater = LayoutInflater.from(mContext)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavsViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
         val binding = FavCardBinding.inflate(layoutInflater, parent,false)
-        return FavCardHolder(binding)
+        return FavsViewHolder(binding)
     }
 
     override fun getItemCount(): Int {
         return favList.size
     }
 
-    override fun onBindViewHolder(holder: FavCardHolder, position: Int) {
-        val food = favList[position]
-        val b = holder.binding
-
-        Glide.with(mContext).load("http://kasimadalan.pe.hu/yemekler/resimler/${food.foodImageName}").into(b.imageViewFavCard)
-        b.textViewNameFavCard.text = food.foodName
-        b.textViewPriceFavCard.text = b.root.context.getString(R.string.price_text, food.foodPrice)
-        b.buttonAddToCartFavCard.setOnClickListener {
-        }
+    override fun onBindViewHolder(holder: FavsViewHolder, position: Int) {
+        holder.bind(favList[position])
     }
 }
---
+++ app/src/main/java/com/cevdetkilickeser/neyicez/presentation/ui/adapter/FavsViewHolder.kt
@@ -0,0 +1,17 @@
+package com.cevdetkilickeser.neyicez.presentation.ui.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.cevdetkilickeser.neyicez.R
+import com.cevdetkilickeser.neyicez.data.model.Fav
+import com.cevdetkilickeser.neyicez.databinding.FavCardBinding
+
+class FavsViewHolder(var binding: FavCardBinding) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(fav: Fav) {
+        with(binding) {
+            Glide.with(root).load("http://kasimadalan.pe.hu/yemekler/resimler/${fav.foodImageName}").into(imageViewFavCard)
+            textViewNameFavCard.text = fav.foodName
+            textViewPriceFavCard.text = root.context.getString(R.string.price_text, fav.foodPrice)
+        }
+    }
+}
