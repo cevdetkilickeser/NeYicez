b9f63d5cb28c421aae545a8b5e248c6043169473
Cevdet Kilickeser <kilickesercevdet@gmail.com>
2024-10-15 16:54:12 +0300

    Fav model class created

---
+++ app/src/main/java/com/cevdetkilickeser/neyicez/data/model/Fav.kt
@@ -0,0 +1,10 @@
+package com.cevdetkilickeser.neyicez.data.model
+
+data class Fav(
+    val id: Int,
+    val username: String,
+    val foodId: Int,
+    val foodName: String,
+    val foodPrice: Int,
+    val foodImageName: String
+)
