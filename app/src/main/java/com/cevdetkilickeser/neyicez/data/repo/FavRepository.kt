6b695d974fc653aef5ae4bb5d9a300d397a74c56
Cevdet Kilickeser <kilickesercevdet@gmail.com>
2024-10-15 16:56:59 +0300

    getFavs fun added to FavRepository and firebase db service injected

--- app/src/main/java/com/cevdetkilickeser/neyicez/data/repo/FavRepository.kt
+++ app/src/main/java/com/cevdetkilickeser/neyicez/data/repo/FavRepository.kt
@@ -1,8 +1,30 @@
 package com.cevdetkilickeser.neyicez.data.repo
 
 import com.cevdetkilickeser.neyicez.data.datasource.FoodsDataSource
+import com.cevdetkilickeser.neyicez.data.model.Fav
+import com.cevdetkilickeser.neyicez.domain.FirebaseDBService
+import javax.inject.Inject
 
-class FavRepository (var fds: FoodsDataSource){
+class FavRepository @Inject constructor (private val db: FirebaseDBService, private val datasource: FoodsDataSource){
 
-
+    fun getFavs(username: String, onResult: (List<Fav>?) -> Unit) {
+        db.firebaseDB.collection("fav_table")
+            .whereEqualTo("username", username)
+            .get()
+            .addOnSuccessListener { documents ->
+                if (documents.isEmpty) {
+                    onResult(null)
+                } else {
+                    val favList = mutableListOf<Fav>()
+                    for (document in documents) {
+                        val fav = document.toObject(Fav::class.java)
+                        favList.add(fav)
+                    }
+                    onResult(favList)
+                }
+            }
+            .addOnFailureListener {
+                onResult(null)
+            }
+    }
 }
--- app/src/main/java/com/cevdetkilickeser/neyicez/di/DatabaseModule.kt
+++ app/src/main/java/com/cevdetkilickeser/neyicez/di/DatabaseModule.kt
@@ -5,10 +5,10 @@ import com.cevdetkilickeser.neyicez.domain.FirebaseDBServiceImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
+import dagger.hilt.android.components.ActivityComponent
 
 @Module
-@InstallIn(SingletonComponent::class)
+@InstallIn(ActivityComponent::class)
 abstract class DatabaseModule {
 
     @Binds
