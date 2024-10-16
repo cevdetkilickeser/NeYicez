ade31ac8f2e7514bd32509790f30f2ddd2864a2d
Cevdet Kilickeser <kilickesercevdet@gmail.com>
2024-10-16 15:42:55 +0300

    FavsViewModel -> loadFavs func made public to call inside the onResume scope

--- app/src/main/java/com/cevdetkilickeser/neyicez/presentation/viewmodel/FavsViewModel.kt
+++ app/src/main/java/com/cevdetkilickeser/neyicez/presentation/viewmodel/FavsViewModel.kt
@@ -3,18 +3,20 @@ package com.cevdetkilickeser.neyicez.presentation.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.cevdetkilickeser.neyicez.data.model.Fav
 import com.cevdetkilickeser.neyicez.data.repo.CartRepository
 import com.cevdetkilickeser.neyicez.data.repo.FavRepository
 import com.cevdetkilickeser.neyicez.domain.AuthService
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class FavsViewModel @Inject constructor(
     authService: AuthService,
     var cartRepo: CartRepository,
-    private var favrepo: FavRepository
+    private var favRepo: FavRepository
 ) : ViewModel() {
 
     private var username = authService.auth.currentUser?.email.toString()
@@ -22,13 +24,15 @@ class FavsViewModel @Inject constructor(
     private val _favList = MutableLiveData<List<Fav>>()
     val favList: LiveData<List<Fav>> = _favList
 
-    init {
-        loadFavs()
-    }
+    private var _isLoading = MutableLiveData<Boolean>()
+    var isLoading: LiveData<Boolean> = _isLoading
 
-    private fun loadFavs() {
-        favrepo.getFavs(username) {
-            _favList.value = it ?: emptyList()
+    fun loadFavs() {
+        viewModelScope.launch {
+            favRepo.getFavs(username) { favList ->
+                _favList.value = favList ?: emptyList()
+                _isLoading.value = false
+            }
         }
     }
 }
