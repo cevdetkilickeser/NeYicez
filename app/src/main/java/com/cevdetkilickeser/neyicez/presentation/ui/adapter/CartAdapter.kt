8f25232b68b6081463a7e85bf24887da25df3654
Cevdet Kilickeser <kilickesercevdet@gmail.com>
2024-10-16 21:32:25 +0300

    CartAdapter.kt, CartViewHolder -> business logic moved to viewmodel with higher order functions, view holder  moved out

--- app/src/main/java/com/cevdetkilickeser/neyicez/presentation/ui/adapter/CartAdapter.kt
+++ app/src/main/java/com/cevdetkilickeser/neyicez/presentation/ui/adapter/CartAdapter.kt
@@ -3,41 +3,27 @@ package com.cevdetkilickeser.neyicez.presentation.ui.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.cevdetkilickeser.neyicez.R
 import com.cevdetkilickeser.neyicez.data.model.Cart
 import com.cevdetkilickeser.neyicez.databinding.CartCardBinding
-import com.cevdetkilickeser.neyicez.presentation.viewmodel.CartViewModel
 
-class CartAdapter(private var cartList: List<Cart>, var viewModel: CartViewModel) :
-    RecyclerView.Adapter<CartAdapter.CartCardHolder>() {
-    inner class CartCardHolder(var binding: CartCardBinding) : RecyclerView.ViewHolder(binding.root)
+class CartAdapter(
+    private var cartList: List<Cart>,
+    private val onClickDeleteButton: (Cart) -> Unit,
+    private val calculateCartItemTotal: (Int, Int) -> Int
+) : RecyclerView.Adapter<CartViewHolder>() {
 
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CartCardHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CartViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         val binding = CartCardBinding.inflate(layoutInflater)
-        return CartCardHolder(binding)
+        return CartViewHolder(binding)
     }
 
     override fun getItemCount(): Int {
         return cartList.size
     }
 
-    override fun onBindViewHolder(holder: CartCardHolder, position: Int) {
-        val cart = cartList[position]
-        val b = holder.binding
-        val totalPrc = cart.foodPrice * cart.foodOrderQuantity
-
-        Glide.with(b.root).load("http://kasimadalan.pe.hu/yemekler/resimler/${cart.foodImageName}")
-            .into(b.imageViewCartCard)
-        b.textViewNameCartCard.text = cart.foodName
-        b.textViewPriceCartCard.text = b.root.context.getString(R.string.price_text, cart.foodPrice)
-        b.textViewQtyCartCard.text = cart.foodOrderQuantity.toString()
-        b.textViewTotalPrcCartCard.text =  b.root.context.getString(R.string.price_text, totalPrc)
-
-        b.imageViewDeleteCartCard.setOnClickListener {
-            viewModel.deleteFromCart(cart.cartFoodId)
-        }
+    override fun onBindViewHolder(holder: CartViewHolder, position: Int) {
+        holder.bind(cartList[position], onClickDeleteButton, calculateCartItemTotal)
     }
 }
---
+++ app/src/main/java/com/cevdetkilickeser/neyicez/presentation/ui/adapter/CartViewHolder.kt
@@ -0,0 +1,31 @@
+package com.cevdetkilickeser.neyicez.presentation.ui.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.cevdetkilickeser.neyicez.R
+import com.cevdetkilickeser.neyicez.data.model.Cart
+import com.cevdetkilickeser.neyicez.databinding.CartCardBinding
+
+class CartViewHolder(var binding: CartCardBinding) : RecyclerView.ViewHolder(binding.root) {
+    inline fun bind(
+        cart: Cart,
+        crossinline onClickDeleteButton: (Cart) -> Unit,
+        calculateCartItemTotal: (Int, Int) -> Int
+    ) {
+        val totalPrc = calculateCartItemTotal(cart.foodPrice, cart.foodOrderQuantity)
+
+        with(binding) {
+            Glide.with(root)
+                .load("http://kasimadalan.pe.hu/yemekler/resimler/${cart.foodImageName}")
+                .into(imageViewCartCard)
+            textViewNameCartCard.text = cart.foodName
+            textViewPriceCartCard.text = root.context.getString(R.string.price_text, cart.foodPrice)
+            textViewQtyCartCard.text = cart.foodOrderQuantity.toString()
+            textViewTotalPrcCartCard.text = root.context.getString(R.string.price_text, totalPrc)
+
+            imageViewDeleteCartCard.setOnClickListener {
+                onClickDeleteButton.invoke(cart)
+            }
+        }
+    }
+}
