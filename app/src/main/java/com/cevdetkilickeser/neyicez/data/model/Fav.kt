a8e36a3638a5ecd731f9e75a6c1113f41073932e
Cevdet Kilickeser <kilickesercevdet@gmail.com>
2024-10-16 15:33:52 +0300

    Fav model class rearranged

--- app/src/main/java/com/cevdetkilickeser/neyicez/data/model/Fav.kt
+++ app/src/main/java/com/cevdetkilickeser/neyicez/data/model/Fav.kt
@@ -1,10 +1,9 @@
 package com.cevdetkilickeser.neyicez.data.model
 
 data class Fav(
-    val id: Int,
-    val username: String,
-    val foodId: Int,
-    val foodName: String,
-    val foodPrice: Int,
-    val foodImageName: String
+    val username: String = "",
+    val foodId: Int = 0,
+    val foodName: String = "",
+    val foodPrice: Int = 0,
+    val foodImageName: String = ""
 )
