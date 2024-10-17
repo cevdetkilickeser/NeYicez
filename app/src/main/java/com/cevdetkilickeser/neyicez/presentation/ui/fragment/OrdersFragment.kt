8b3d78d9e3971117833a99a1c3872559644e57ee
Cevdet Kilickeser <kilickesercevdet@gmail.com>
2024-10-17 14:28:05 +0300

    OrdersFragment.kt -> initObservers added

--- app/src/main/java/com/cevdetkilickeser/neyicez/presentation/ui/fragment/OrdersFragment.kt
+++ app/src/main/java/com/cevdetkilickeser/neyicez/presentation/ui/fragment/OrdersFragment.kt
@@ -1,23 +1,30 @@
 package com.cevdetkilickeser.neyicez.presentation.ui.fragment
 
+import android.graphics.Rect
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.RecyclerView
 import com.cevdetkilickeser.neyicez.databinding.FragmentOrdersBinding
+import com.cevdetkilickeser.neyicez.presentation.ui.adapter.OrdersAdapter
 import com.cevdetkilickeser.neyicez.presentation.viewmodel.OrdersViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class OrdersFragment : Fragment() {
     private lateinit var binding: FragmentOrdersBinding
     private lateinit var viewModel: OrdersViewModel
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         binding = FragmentOrdersBinding.inflate(inflater, container, false)
 
-        //viewModel.orderList.observe(viewLifecycleOwner){
-        //    val orderAdapter = OrdersAdapter(requireContext(),it,viewModel)
-
-        //}
+        initObservers()
 
         return binding.root
     }
@@ -25,8 +32,39 @@ class OrdersFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //val tempViewModel: OrdersViewModel by viewModels()
-        //viewModel = tempViewModel
+        val tempViewModel: OrdersViewModel by viewModels()
+        viewModel = tempViewModel
     }
 
+    override fun onResume() {
+        super.onResume()
+        viewModel.loadOrders()
+    }
+
+    private fun initObservers() {
+        viewModel.orderList.observe(viewLifecycleOwner) {
+            val ordersAdapter = OrdersAdapter(it)
+            binding.rvOrders.apply {
+                adapter = ordersAdapter
+                addItemDecoration(object : RecyclerView.ItemDecoration() {
+                    override fun getItemOffsets(
+                        outRect: Rect,
+                        view: View,
+                        parent: RecyclerView,
+                        state: RecyclerView.State
+                    ) {
+                        outRect.set(0, 0, 0, 32)
+                    }
+                })
+            }
+        }
+
+        viewModel.isLoading.observe(viewLifecycleOwner) { isLoading ->
+            if (isLoading) {
+                binding.progressBar.visibility = View.VISIBLE
+            } else {
+                binding.progressBar.visibility = View.GONE
+            }
+        }
+    }
 }
