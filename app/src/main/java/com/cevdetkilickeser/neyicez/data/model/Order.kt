199ade833f64375183fb4db11b648d4f54cd0bf9
Cevdet Kilickeser <kilickesercevdet@gmail.com>
2024-10-17 14:21:02 +0300

    Order.kt -> order model class rearranged

--- app/src/main/java/com/cevdetkilickeser/neyicez/data/model/Order.kt
+++ app/src/main/java/com/cevdetkilickeser/neyicez/data/model/Order.kt
@@ -1,6 +1,9 @@
 package com.cevdetkilickeser.neyicez.data.model
 
 data class Order (
-    val cartList: List<Cart>,
-    val username: String
+    val id: String? = null,
+    val username: String = "",
+    val orderDate: String = "",
+    val cartList: List<Cart> = emptyList(),
+    val orderTotal: String = ""
 )
