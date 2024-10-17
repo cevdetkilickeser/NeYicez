5de39f3e1f59a447aef9b04fcc044fef0897f9fc
Cevdet Kilickeser <kilickesercevdet@gmail.com>
2024-10-17 08:40:59 +0300

    CartRepository.kt -> approveOrder function rearranged

--- app/src/main/java/com/cevdetkilickeser/neyicez/data/repo/CartRepository.kt
+++ app/src/main/java/com/cevdetkilickeser/neyicez/data/repo/CartRepository.kt
@@ -3,36 +3,33 @@ package com.cevdetkilickeser.neyicez.data.repo
 import com.cevdetkilickeser.neyicez.data.datasource.FoodsDataSource
 import com.cevdetkilickeser.neyicez.data.model.Cart
 import com.cevdetkilickeser.neyicez.data.model.Food
+import com.cevdetkilickeser.neyicez.data.model.Order
+import com.cevdetkilickeser.neyicez.domain.FirebaseDBService
 import javax.inject.Inject
 
-class CartRepository  @Inject constructor (private var dataSource: FoodsDataSource){
+class CartRepository  @Inject constructor (private val db: FirebaseDBService, private var dataSource: FoodsDataSource){
 
     suspend fun loadCart(username: String) : List<Cart> = dataSource.loadCart(username)
 
     suspend fun deleteFromCart(cartFoodId:Int, username: String) = dataSource.deleteFromCart(cartFoodId,username)
 
-
-    suspend fun approveOrder(order:List<Cart>) = dataSource.approveOrder(order)
-
-    suspend fun loadOrders(username: String) = dataSource.loadOrders(username)
-
     suspend fun addToCart(food: Food, foodOrderQuantity: Int, username: String) {
-        var existingItem: Cart? = null
+        var existingCartItem: Cart? = null
         try {
             val cartFoods = loadCart(username)
-            existingItem = cartFoods.find { it.foodName == food.foodName }
+            existingCartItem = cartFoods.find { it.foodName == food.foodName }
         } catch (_: Exception) {}
 
-        existingItem?.let {
-            addToCartExistFood(food, foodOrderQuantity, existingItem, username)
+        existingCartItem?.let {
+            addToCartExistItem(food, foodOrderQuantity, it, username)
         } ?: run {
-            addToCartNewFood(food, foodOrderQuantity, username)
+            addToCartNewItem(food, foodOrderQuantity, username)
         }
     }
 
-    private suspend fun addToCartExistFood(food: Food, foodOrderQuantity: Int, existingItem: Cart, username: String) {
-        val newQuantity = existingItem.foodOrderQuantity + foodOrderQuantity
-        deleteFromCart(existingItem.cartFoodId, username)
+    private suspend fun addToCartExistItem(food: Food, foodOrderQuantity: Int, existingCartItem: Cart, username: String) {
+        val newQuantity = existingCartItem.foodOrderQuantity + foodOrderQuantity
+        deleteFromCart(existingCartItem.cartFoodId, username)
         dataSource.addToCard(
             food.foodName,
             food.foodImageName,
@@ -42,7 +39,7 @@ class CartRepository  @Inject constructor (private var dataSource: FoodsDataSour
         )
     }
 
-    private suspend fun addToCartNewFood(food: Food, foodOrderQuantity: Int, username: String) {
+    private suspend fun addToCartNewItem(food: Food, foodOrderQuantity: Int, username: String) {
         dataSource.addToCard(
             food.foodName,
             food.foodImageName,
@@ -50,6 +47,18 @@ class CartRepository  @Inject constructor (private var dataSource: FoodsDataSour
             foodOrderQuantity,
             username
         )
+    }
+
+    suspend fun approveOrder(cartList: List<Cart>, username: String) {
+        for (cart in cartList) {
+            deleteFromCart(cart.cartFoodId, cart.username)
+        }
+
+        val order = Order(cartList, username)
+        db.firebaseDB.collection("orderCollection")
+            .add(order)
+            .addOnSuccessListener { }
+            .addOnFailureListener { }
     }
 
 }
