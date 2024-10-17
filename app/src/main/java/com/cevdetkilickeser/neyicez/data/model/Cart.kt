b5af3683d62490cdccb97f372154e8ac2ae89c65
Cevdet Kilickeser <kilickesercevdet@gmail.com>
2024-10-17 14:16:35 +0300

    Cart.kt -> default values added

--- app/src/main/java/com/cevdetkilickeser/neyicez/data/model/Cart.kt
+++ app/src/main/java/com/cevdetkilickeser/neyicez/data/model/Cart.kt
@@ -4,10 +4,10 @@ import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 
 data class Cart (
-    @SerializedName ("sepet_yemek_id") val cartFoodId: Int,
-    @SerializedName ("yemek_adi") val foodName: String,
-    @SerializedName ("yemek_resim_adi") val foodImageName: String,
-    @SerializedName ("yemek_fiyat") val foodPrice: Int,
-    @SerializedName ("yemek_siparis_adet") val foodOrderQuantity: Int,
-    @SerializedName ("kullanici_adi") val username: String
+    @SerializedName ("sepet_yemek_id") val cartFoodId: Int = 0,
+    @SerializedName ("yemek_adi") val foodName: String = "",
+    @SerializedName ("yemek_resim_adi") val foodImageName: String = "",
+    @SerializedName ("yemek_fiyat") val foodPrice: Int = 0,
+    @SerializedName ("yemek_siparis_adet") val foodOrderQuantity: Int = 0,
+    @SerializedName ("kullanici_adi") val username: String = ""
 ) : Serializable
