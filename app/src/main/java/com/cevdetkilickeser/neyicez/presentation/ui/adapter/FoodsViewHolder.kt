2f710d275acfd1e35369228a64518172b40cc3c3
Cevdet Kilickeser <kilickesercevdet@gmail.com>
2024-10-17 14:32:30 +0300

    FoodsViewHolder.kt -> FoodsCardBinding renamed to ItemViewFoodBinding

--- app/src/main/java/com/cevdetkilickeser/neyicez/presentation/ui/adapter/FoodsViewHolder.kt
+++ app/src/main/java/com/cevdetkilickeser/neyicez/presentation/ui/adapter/FoodsViewHolder.kt
@@ -4,17 +4,22 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.cevdetkilickeser.neyicez.R
 import com.cevdetkilickeser.neyicez.data.model.Food
-import com.cevdetkilickeser.neyicez.databinding.FoodsCardBinding
+import com.cevdetkilickeser.neyicez.databinding.ItemViewFoodBinding
 
-class FoodsViewHolder(val binding: FoodsCardBinding) : RecyclerView.ViewHolder(binding.root) {
+class FoodsViewHolder(val binding: ItemViewFoodBinding) : RecyclerView.ViewHolder(binding.root) {
 
-    inline fun bind(food: Food, crossinline onFoodClickListener: (Food) -> Unit, crossinline onAddToCartClickListener: (Food) -> Unit) {
+    inline fun bind(
+        food: Food,
+        crossinline onFoodClickListener: (Food) -> Unit,
+        crossinline onAddToCartClickListener: (Food) -> Unit
+    ) {
         with(binding) {
             Glide.with(root.context)
                 .load("http://kasimadalan.pe.hu/yemekler/resimler/${food.foodImageName}")
                 .into(imageViewFoodsCard)
             textViewNameFoodsCard.text = food.foodName
-            textViewPriceFoodsCard.text = root.context.getString(R.string.price_text, food.foodPrice)
+            textViewPriceFoodsCard.text =
+                root.context.getString(R.string.price_text, food.foodPrice)
             buttonAddFoodsCard.setOnClickListener {
                 onAddToCartClickListener.invoke(food)
             }
