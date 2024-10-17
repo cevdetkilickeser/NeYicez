fe15788b630c25b61751102e46c3222488e71913
Cevdet Kilickeser <kilickesercevdet@gmail.com>
2024-10-17 16:00:29 +0300

    Ext.kt -> onClickBackButton and getCurrentDateTime functions added

--- app/src/main/java/com/cevdetkilickeser/neyicez/utils/Ext.kt
+++ app/src/main/java/com/cevdetkilickeser/neyicez/utils/Ext.kt
@@ -1,8 +1,13 @@
 package com.cevdetkilickeser.neyicez.utils
 
 import android.view.View
+import androidx.fragment.app.Fragment
 import androidx.navigation.NavDirections
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
+import com.google.firebase.Timestamp
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 fun navigate(view: View, action: Int) {
     Navigation.findNavController(view).navigate(action)
@@ -10,4 +15,15 @@ fun navigate(view: View, action: Int) {
 
 fun navigate(view: View, action: NavDirections) {
     Navigation.findNavController(view).navigate(action)
+}
+
+fun onClickBackButton(fragment: Fragment) {
+    findNavController(fragment).popBackStack()
+}
+
+fun getCurrentDateTime(timestamp: Timestamp): String{
+    val dateFormat = SimpleDateFormat("dd/MM/yyyy - HH:mm", Locale.getDefault())
+    val date = timestamp.toDate()
+    val currentDateTime = dateFormat.format(date)
+    return currentDateTime
 }
