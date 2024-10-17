479551f0a720d058ed0c4e7428effa054e191f70
Cevdet Kilickeser <kilickesercevdet@gmail.com>
2024-10-17 16:17:20 +0300

    Order.kt -> orderDate property type changed to Timestamp

--- app/src/main/java/com/cevdetkilickeser/neyicez/data/model/Order.kt
+++ app/src/main/java/com/cevdetkilickeser/neyicez/data/model/Order.kt
@@ -1,9 +1,11 @@
 package com.cevdetkilickeser.neyicez.data.model
 
+import com.google.firebase.Timestamp
+
 data class Order (
     val id: String? = null,
     val username: String = "",
-    val orderDate: String = "",
     val cartList: List<Cart> = emptyList(),
-    val orderTotal: String = ""
+    val orderTotal: String = "",
+    val orderDate: Timestamp = Timestamp.now()
 )
