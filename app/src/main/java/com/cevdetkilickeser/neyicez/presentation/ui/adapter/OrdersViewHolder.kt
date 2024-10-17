aead503e7b0eabca6dc38767dda8cb23d552f42c
Cevdet Kilickeser <kilickesercevdet@gmail.com>
2024-10-17 16:17:43 +0300

    OrdersViewHolder.kt -> getCurrentDateTime fun added to convert date to string

--- app/src/main/java/com/cevdetkilickeser/neyicez/presentation/ui/adapter/OrdersViewHolder.kt
+++ app/src/main/java/com/cevdetkilickeser/neyicez/presentation/ui/adapter/OrdersViewHolder.kt
@@ -3,12 +3,13 @@ package com.cevdetkilickeser.neyicez.presentation.ui.adapter
 import androidx.recyclerview.widget.RecyclerView
 import com.cevdetkilickeser.neyicez.data.model.Order
 import com.cevdetkilickeser.neyicez.databinding.ItemViewOrderBinding
+import com.cevdetkilickeser.neyicez.utils.getCurrentDateTime
 
 class OrdersViewHolder(var binding: ItemViewOrderBinding) : RecyclerView.ViewHolder(binding.root) {
     fun bind(order: Order) {
         with(binding) {
             orderNumber.text = order.id
-            orderDate.text = order.orderDate
+            orderDate.text = getCurrentDateTime(order.orderDate)
             orderTotal.text = order.orderTotal
         }
     }
