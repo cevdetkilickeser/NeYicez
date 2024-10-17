1f420549ec1934ccec07a43e30de1a6c224101b8
Cevdet Kilickeser <kilickesercevdet@gmail.com>
2024-10-17 14:21:29 +0300

    OrdersAdapter.kt -> OrdersAdapter rearranged

--- app/src/main/java/com/cevdetkilickeser/neyicez/presentation/ui/adapter/OrdersAdapter.kt
+++ app/src/main/java/com/cevdetkilickeser/neyicez/presentation/ui/adapter/OrdersAdapter.kt
@@ -1,30 +1,26 @@
 package com.cevdetkilickeser.neyicez.presentation.ui.adapter
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.cevdetkilickeser.neyicez.data.model.Order
-import com.cevdetkilickeser.neyicez.databinding.CartCardBinding
-import com.cevdetkilickeser.neyicez.presentation.viewmodel.OrdersViewModel
+import com.cevdetkilickeser.neyicez.databinding.ItemViewOrderBinding
 
-class OrdersAdapter (var mContext: Context, var orderList: List<Order>, var viewModel: OrdersViewModel) :
-RecyclerView.Adapter<OrdersAdapter.OrdersCardHolder>() {
-    inner class OrdersCardHolder(var binding: CartCardBinding) : RecyclerView.ViewHolder(binding.root)
+class OrdersAdapter(private var orderList: List<Order>) :
+    RecyclerView.Adapter<OrdersViewHolder>() {
 
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OrdersCardHolder {
-        val layoutInflater = LayoutInflater.from(mContext)
-        val binding = CartCardBinding.inflate(layoutInflater)
-        return OrdersCardHolder(binding)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OrdersViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = ItemViewOrderBinding.inflate(layoutInflater)
+        return OrdersViewHolder(binding)
     }
 
     override fun getItemCount(): Int {
         return orderList.size
     }
 
-    override fun onBindViewHolder(holder: OrdersCardHolder, position: Int) {
-        val order = orderList.get(position)
-        val b = holder.binding
+    override fun onBindViewHolder(holder: OrdersViewHolder, position: Int) {
+        holder.bind(orderList[position])
     }
 }
